2c3cee07fb0468147086fa1777d697b6255408b9
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-10-27 07:26:39 +0430

    Handling Forms Without Reload (AJAX + Fetch Basics) Basic Fetch Example

--- form handling/script.js
+++ form handling/script.js
@@ -1,39 +1,48 @@
-const addFieldBtn = document.querySelector('#addField');
-const container = document.querySelector('#fieldsContainer');
+// const addFieldBtn = document.querySelector('#addField');
+// const container = document.querySelector('#fieldsContainer');
 
-let count = 0;
+// let count = 0;
 
-addFieldBtn.addEventListener("click", () => {
-    count++;
-    const input = document.createElement('input');
-    input.type = "text";
-    input.placeholder = `Field ${count}`;
-    input.classList.add('form-field');
+// addFieldBtn.addEventListener("click", () => {
+//     count++;
+//     const input = document.createElement('input');
+//     input.type = "text";
+//     input.placeholder = `Field ${count}`;
+//     input.classList.add('form-field');
     
-    container.appendChild(input);
-});
-
-const submitBtn = document.querySelector('#submitForm');
-
-// submitBtn.addEventListener("click", () => {
-//     const values = [...document.querySelectorAll('.form-field')].map(
-//         (field) => field.value
-//     );
-//     console.log("Form Data: ",values);
+//     container.appendChild(input);
 // });
 
-submitBtn.addEventListener("click", () => {
-  const fields = document.querySelectorAll(".form-field");
-  let valid = true;
+// const submitBtn = document.querySelector('#submitForm');
 
-  fields.forEach((field) => {
-    if (field.value.trim() === "") {
-      field.style.border = "2px solid red";
-      valid = false;
-    } else {
-      field.style.border = "2px solid green";
-    }
-  });
+// // submitBtn.addEventListener("click", () => {
+// //     const values = [...document.querySelectorAll('.form-field')].map(
+// //         (field) => field.value
+// //     );
+// //     console.log("Form Data: ",values);
+// // });
 
-  if (valid) alert("All fields valid!");
+// submitBtn.addEventListener("click", () => {
+//   const fields = document.querySelectorAll(".form-field");
+//   let valid = true;
+
+//   fields.forEach((field) => {
+//     if (field.value.trim() === "") {
+//       field.style.border = "2px solid red";
+//       valid = false;
+//     } else {
+//       field.style.border = "2px solid green";
+//     }
+//   });
+
+//   if (valid) alert("All fields valid!");
+// });
+
+
+// Basic Fetch Example
+document.getElementById('fetchBtn').addEventListener("click",() => {
+  fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => console.log("Fetch Post: ",data))
+  .catch((err) => console.error("Error: ",err));
 });
