2e17d69af3a1690968e86fdf7e3a898a6e68f703
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-11-18 07:59:30 +0430

    lecture default parameter or fallback values

--- ES6/script.js
+++ ES6/script.js
@@ -1,56 +1,64 @@
-// // // // // for (let i = 0; i < 3; i++) {
-// // // // //   setTimeout(() => console.log(i), 100);
-// // // // // }
+// // // // // // for (let i = 0; i < 3; i++) {
+// // // // // //   setTimeout(() => console.log(i), 100);
+// // // // // // }
 
 
-// // // // // Destructuring
-// // // // const colors = ["white","black","red","green","blue"];
-// // // // // console.log(colors[3]);
-// // // // const [a,b,c,d,e] = colors;
-// // // // console.log(e);
+// // // // // // Destructuring
+// // // // // const colors = ["white","black","red","green","blue"];
+// // // // // // console.log(colors[3]);
+// // // // // const [a,b,c,d,e] = colors;
+// // // // // console.log(e);
 
-// // // const person = {
-// // //   name: "Ali",
-// // //   age: 25,
-// // //   country: "Afghanistan",
-// // //   city: "Herat"
-// // // };
+// // // // const person = {
+// // // //   name: "Ali",
+// // // //   age: 25,
+// // // //   country: "Afghanistan",
+// // // //   city: "Herat"
+// // // // };
 
-// // // // console.log(person.name);
-// // // const {name,age,country} = person;
-// // // // console.log(name);
-// // // // console.log(age);
-// // // // console.log(country);
-// // // const {name: firstname, age:userage, city = "Kabul"} = person;
-// // // console.log(firstname);
-// // // console.log(city);
-
-
-// // let arr1 = [1,2,3];
-// // let arr2 = [4,5];
-
-// // console.log(arr1);
-// // console.log(arr2);
-
-// // // spread operator
-// // let combine = [...arr1,...arr2];
-// // console.log(combine);
-
-// const obj1 = { name: "Ali" };
-// const obj2 = { age: 20 };
-// const merged = { ...obj1, ...obj2 };
-// console.log(merged); 
+// // // // // console.log(person.name);
+// // // // const {name,age,country} = person;
+// // // // // console.log(name);
+// // // // // console.log(age);
+// // // // // console.log(country);
+// // // // const {name: firstname, age:userage, city = "Kabul"} = person;
+// // // // console.log(firstname);
+// // // // console.log(city);
 
 
-// const [x,y,...z] = [10,20,30,40,50];
-// console.log(z);
+// // // let arr1 = [1,2,3];
+// // // let arr2 = [4,5];
 
-// const { name, ...details } = { name: "Sara", age: 22, country: "Afghanistan" };
-// console.log(details); 
+// // // console.log(arr1);
+// // // console.log(arr2);
+
+// // // // spread operator
+// // // let combine = [...arr1,...arr2];
+// // // console.log(combine);
+
+// // const obj1 = { name: "Ali" };
+// // const obj2 = { age: 20 };
+// // const merged = { ...obj1, ...obj2 };
+// // console.log(merged); 
 
 
-function sum(...numbers) {
-  return numbers.reduce((acc, n) => acc + n);
+// // const [x,y,...z] = [10,20,30,40,50];
+// // console.log(z);
+
+// // const { name, ...details } = { name: "Sara", age: 22, country: "Afghanistan" };
+// // console.log(details); 
+
+
+// function sum(...numbers) {
+//   return numbers.reduce((acc, n) => acc + n);
+// }
+
+// console.log(sum(1, 2, 3, 4, 5)); 
+
+
+function greet(name = "Guest") {
+  console.log(`Hello, ${name}!`);
 }
 
-console.log(sum(1, 2, 3, 4, 5)); 
+greet(); // "Hello, Guest!"  fallback active and show the default value
+greet("Ali"); // "Hello, Ali!"
