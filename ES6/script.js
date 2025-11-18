2c982f61c9d77811faed73651d1d3d10ee6993d4
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-11-18 07:43:42 +0430

    lecture Spread operator or three dots

--- ES6/script.js
+++ ES6/script.js
@@ -1,30 +1,45 @@
-// // for (let i = 0; i < 3; i++) {
-// //   setTimeout(() => console.log(i), 100);
-// // }
+// // // // for (let i = 0; i < 3; i++) {
+// // // //   setTimeout(() => console.log(i), 100);
+// // // // }
 
 
-// // Destructuring
-// const colors = ["white","black","red","green","blue"];
-// // console.log(colors[3]);
-// const [a,b,c,d,e] = colors;
-// console.log(e);
+// // // // Destructuring
+// // // const colors = ["white","black","red","green","blue"];
+// // // // console.log(colors[3]);
+// // // const [a,b,c,d,e] = colors;
+// // // console.log(e);
 
-const person = {
-  name: "Ali",
-  age: 25,
-  country: "Afghanistan",
-  city: "Herat"
-};
+// // const person = {
+// //   name: "Ali",
+// //   age: 25,
+// //   country: "Afghanistan",
+// //   city: "Herat"
+// // };
 
-// console.log(person.name);
-const {name,age,country} = person;
-// console.log(name);
-// console.log(age);
-// console.log(country);
-const {name: firstname, age:userage, city = "Kabul"} = person;
-console.log(firstname);
-console.log(city);
+// // // console.log(person.name);
+// // const {name,age,country} = person;
+// // // console.log(name);
+// // // console.log(age);
+// // // console.log(country);
+// // const {name: firstname, age:userage, city = "Kabul"} = person;
+// // console.log(firstname);
+// // console.log(city);
 
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5];
+
+// console.log(arr1);
+// console.log(arr2);
+
+// // spread operator
+// let combine = [...arr1,...arr2];
+// console.log(combine);
+
+const obj1 = { name: "Ali" };
+const obj2 = { age: 20 };
+const merged = { ...obj1, ...obj2 };
+console.log(merged); 
 
 
 
